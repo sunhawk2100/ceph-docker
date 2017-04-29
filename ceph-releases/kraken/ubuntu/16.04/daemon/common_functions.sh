7fa9eb8f8630390e3feefe472ab46443b7771885
Sébastien Han <seb@redhat.com>
2017-04-29 19:21:19 +0200

    daemon: fix all the shellcheck issues
    Use local variables for functions.
    Be more strick and follow best bash practices. Using 'local' to declare
    a variable when its scope is limited to the function it's part of only.
    
    Fixing some shell specific things as well. Addressing all the shellcheck
    issues.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- ceph-releases/kraken/ubuntu/16.04/daemon/common_functions.sh
+++ ceph-releases/kraken/ubuntu/16.04/daemon/common_functions.sh
@@ -7,8 +7,9 @@ function log {
     return 1
   fi
 
-  TIMESTAMP=$(date '+%F %T')
-  echo "${TIMESTAMP}  $0: $*"
+  local timestamp
+  timestamp=$(date '+%F %T')
+  echo "$timestamp  $0: $*"
   return 0
 }
 
@@ -23,14 +24,15 @@ function check_config {
 # ceph admin key exists or die
 function check_admin_key {
   if [[ ! -e $ADMIN_KEYRING ]]; then
-      log "ERROR- $ADMIN_KEYRING must exist; get it from your existing mon"
-      exit 1
+    log "ERROR- $ADMIN_KEYRING must exist; get it from your existing mon"
+    exit 1
   fi
 }
 
 # Given two strings, return the length of the shared prefix
 function prefix_length {
-  local maxlen=${#1}
+  local maxlen
+  maxlen=${#1}
   for ((i=maxlen-1;i>=0;i--)); do
     if [[ "${1:0:i}" == "${2:0:i}" ]]; then
       echo $i
@@ -41,14 +43,14 @@ function prefix_length {
 
 # Test if a command line tool is available
 function is_available {
-  command -v $@ &>/dev/null
+  command -v "$@" &>/dev/null
 }
 
 # create the mandatory directories
 function create_mandatory_directories {
   # Let's create the bootstrap directories
   for keyring in $OSD_BOOTSTRAP_KEYRING $MDS_BOOTSTRAP_KEYRING $RGW_BOOTSTRAP_KEYRING; do
-    mkdir -p $(dirname $keyring)
+    mkdir -p "$(dirname "$keyring")"
   done
 
   # Let's create the ceph directories
@@ -63,13 +65,13 @@ function create_mandatory_directories {
   mkdir -p /var/run/ceph
 
   # Creating rados directories
-  mkdir -p /var/lib/ceph/radosgw/${RGW_NAME}
+  mkdir -p /var/lib/ceph/radosgw/"${RGW_NAME}"
 
   # Create the MDS directory
-  mkdir -p /var/lib/ceph/mds/${CLUSTER}-${MDS_NAME}
+  mkdir -p /var/lib/ceph/mds/"${CLUSTER}-${MDS_NAME}"
 
   # Create the MGR directory
-  mkdir -p /var/lib/ceph/mgr/${CLUSTER}-$MGR_NAME
+  mkdir -p /var/lib/ceph/mgr/"${CLUSTER}-$MGR_NAME"
 
   # Adjust the owner of all those directories
   chown --verbose -R ceph. /var/run/ceph/ /var/lib/ceph/*
@@ -87,8 +89,10 @@ function dev_part {
 
   if [[ -L ${osd_device} ]]; then
     # This device is a symlink. Work out it's actual device
-    local actual_device=$(resolve_symlink ${osd_device})
-    local bn=$(basename ${osd_device})
+    local actual_device
+    actual_device=$(readlink -f "${osd_device}")
+    local bn
+    bn=$(basename "${osd_device}")
     if [[ "${actual_device:0-1:1}" == [0-9] ]]; then
       local desired_partition="${actual_device}p${osd_partition}"
     else
@@ -97,17 +101,20 @@ function dev_part {
     # Now search for a symlink in the directory of $osd_device
     # that has the correct desired partition, and the longest
     # shared prefix with the original symlink
-    local symdir=$(dirname ${osd_device})
+    local symdir
+    symdir=$(dirname "${osd_device}")
     local link=""
     local pfxlen=0
-    for option in $(ls $symdir); do
-    if [[ $(resolve_symlink $symdir/$option) == $desired_partition ]]; then
-      local optprefixlen=$(prefix_length $option $bn)
-      if [[ $optprefixlen > $pfxlen ]]; then
-        link=$symdir/$option
-        pfxlen=$optprefixlen
+    for option in ${symdir}*; do
+      [[ -e $option ]] || break
+      if [[ $(readlink -f "$symdir"/"$option") == "$desired_partition" ]]; then
+        local optprefixlen
+        optprefixlen=$(prefix_length "$option" "$bn")
+        if [[ $optprefixlen > $pfxlen ]]; then
+          link=$symdir/$option
+          pfxlen=$optprefixlen
+        fi
       fi
-    fi
     done
     if [[ $pfxlen -eq 0 ]]; then
       >&2 log "Could not locate appropriate symlink for partition ${osd_partition} of ${osd_device}"
@@ -122,11 +129,12 @@ function dev_part {
 }
 
 function osd_trying_to_determine_scenario {
-  if [ -z "${OSD_DEVICE}" ]; then
+  ${OSD_DEVICE:-none}
+  if [[ ${OSD_DEVICE} == "none" ]]; then
     log "Bootstrapped OSD(s) found; using OSD directory"
     source osd_directory.sh
     osd_directory
-  elif $(parted --script ${OSD_DEVICE} print | egrep -sq '^ 1.*ceph data'); then
+  elif parted --script "${OSD_DEVICE}" print | grep -sqE '^ 1.*ceph data'; then
     log "Bootstrapped OSD found; activating ${OSD_DEVICE}"
     source osd_disk_activate.sh
     osd_activate
@@ -138,11 +146,12 @@ function osd_trying_to_determine_scenario {
 }
 
 function get_osd_dev {
-  for i in ${OSD_DISKS}
-   do
-    osd_id=$(echo ${i}|sed 's/\(.*\):\(.*\)/\1/')
-    osd_dev="/dev/$(echo ${i}|sed 's/\(.*\):\(.*\)/\2/')"
-    if [ ${osd_id} = ${1} ]; then
+  for i in ${OSD_DISKS}; do
+    local osd_id
+    osd_id=$(echo "${i}"|sed 's/\(.*\):\(.*\)/\1/')
+    local osd_dev
+    osd_dev="/dev/$(echo "${i}"|sed 's/\(.*\):\(.*\)/\2/')"
+    if [[ "${osd_id}" == "${1}" ]]; then
       echo -n "${osd_dev}"
     fi
   done
@@ -159,7 +168,9 @@ function is_integer {
   # Supports also negative integers
   # We use $@ here to consider everything given as parameter and not only the
   # first one : that's mainly for splited strings like "10 10"
-  [[ $@ =~ ^-?[0-9]+$ ]]
+  for arg in "$@"; do
+    [[ $arg =~ ^-?[0-9]+$ ]]
+  done
 }
 
 # Transform any set of strings to lowercase
@@ -212,7 +223,7 @@ function valid_scenarios {
       ALL_SCENARIOS=${ALL_SCENARIOS/$tag /}
     done
   fi
-  log "Valid values for CEPH_DAEMON are $(to_uppercase $ALL_SCENARIOS)."
+  log "Valid values for CEPH_DAEMON are $(to_uppercase "$ALL_SCENARIOS")."
   log "Valid values for the daemon parameter are $ALL_SCENARIOS"
 }
 
--- ceph-releases/kraken/ubuntu/16.04/daemon/debug.sh
+++ ceph-releases/kraken/ubuntu/16.04/daemon/debug.sh
@@ -7,20 +7,20 @@ function extract_param {
   echo "${1##*=}"
 }
 
-for option in $(comma_to_space ${DEBUG}); do
+for option in $(comma_to_space "${DEBUG}"); do
   case $option in
     verbose)
       echo "VERBOSE: activating bash debugging mode."
       set -x
       ;;
     fstree*)
-      echo "FSTREE: uncompressing content of $(extract_param $option)"
+      echo "FSTREE: uncompressing content of $(extract_param "$option")"
       # NOTE (leseb): the entrypoint should already be running from /
       # This is just a safeguard
       pushd / > /dev/null
 
       # Downloading patched filesystem
-      curl --silent --output patch.tar -L $(extract_param $option)
+      curl --silent --output patch.tar -L "$(extract_param "$option")"
 
       # If the file isn't present, let's stop here
       [ -f patch.tar ]
@@ -28,16 +28,20 @@ for option in $(comma_to_space ${DEBUG}); do
       # Let's find out if the tarball has the / in a sub-directory
       strip_level=0
       for sub_level in $(seq 2 -1 0); do
-        tar -tf patch.tar | cut -d "/" -f $((sub_level+1)) | egrep -sqw "bin|etc|lib|lib64|opt|run|usr|sbin|var" && strip_level=$sub_level || true
+        set +e
+        if tar -tf patch.tar | cut -d "/" -f $((sub_level+1)) | grep -sqwE "bin|etc|lib|lib64|opt|run|usr|sbin|var"; then
+          strip_level=$sub_level
+        fi
+        set -e
       done
       echo "The main directory is at level $strip_level"
       echo ""
       echo "SHA1 of the archive is: $(sha1sum patch.tar)"
       echo ""
       echo "Now, we print the SHA1 of each file."
-      for f in $(tar xfpv patch.tar --show-transformed-names --strip=$strip_level); do
+      for f in $(tar xfpv patch.tar --show-transformed-names --strip="$strip_level"); do
         if [[ ! -d $f ]]; then
-          sha1sum $f
+          sha1sum "$f"
         fi
       done
       rm -f patch.tar
